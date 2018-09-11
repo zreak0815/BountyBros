97871854e7b5317577100e3f09fd1ccacf317ed7
Dennis Maas <dennis.ma@live.de>
2018-09-11 19:20:40 +0200

    Charactersheetscript.

--- Assets/JumpAndRunUIControl.cs
+++ Assets/JumpAndRunUIControl.cs
@@ -8,31 +8,7 @@ public class JumpAndRunUIControl : MonoBehaviour {
    private GameObject levelImage;
    private Text levelLabel;
 
-   // Use this for initialization
-   void Start() {
-      levelImage = GameObject.Find("CharacterSheet");
-      levelImage.SetActive(Displayed);
-   }
-
-   // Update is called once per frame
-   void Update() {
-      if (Input.GetKeyDown(KeyCode.C)) {
-         Displayed = !Displayed;
-         Debug.Log(Displayed);
-         levelImage.SetActive(Displayed);
-      }
-   }
-}
-/*
- using UnityEngine;
-using UnityEngine.UI;
-
-public class CharacterSheetControl : MonoBehaviour {
-   private bool KeyIsPressed = false;
-   private bool Displayed = true;
-
-   private GameObject levelImage;
-   private Text levelLabel;
+   public PlayerValues stats { get; set; }
 
    // Use this for initialization
    void Start() {
@@ -49,4 +25,3 @@ public class CharacterSheetControl : MonoBehaviour {
       }
    }
 }
-    */
