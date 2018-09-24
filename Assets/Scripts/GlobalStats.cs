a717a6b60893aa54635fee47b8e409bdadf740fb
Ole Mauri <cgt101592@stud.fh-wedel.de>
2018-09-24 22:21:42 +0200

    -globale stats fix

--- Assets/Scripts/GlobalStats.cs
+++ Assets/Scripts/GlobalStats.cs
@@ -7,11 +7,6 @@ public class GlobalStats : MonoBehaviour {
     public int exp = 0;
     public int level = 1;
     public int potions = 0;
-    public int attack = 0;
-    public int defense = 0;
-    public int health = 0;
-    public int evade = 0;
-    public int statPoints = 0;
 
     // Use this for initialization
     void Start () {
@@ -22,11 +17,6 @@ public class GlobalStats : MonoBehaviour {
         player.currentXP = exp;
         player.hpFlaskAmount = potions;
         player.setLevel(level);
-        CharSheetController.Lvl = level;
-        CharSheetController.Atk = attack;
-        CharSheetController.Def = defense;
-        CharSheetController.Evs = evade;
-        CharSheetController.StatPoints = statPoints;
         FindObjectOfType<CharSheetController>().updateStats();
         FindObjectOfType<CombatManager>().xpText.text = "XP " + player.getXP() + "/" + player.getXPForLevel(player.getLevel());
     }
@@ -35,10 +25,5 @@ public class GlobalStats : MonoBehaviour {
         exp = player.currentXP;
         potions = player.hpFlaskAmount;
         level = CharSheetController.Lvl;
-        attack = CharSheetController.Atk;
-        defense = CharSheetController.Def;
-        health = CharSheetController.HP;
-        evade = CharSheetController.Evs;
-        statPoints = CharSheetController.StatPoints;
     }
 }
